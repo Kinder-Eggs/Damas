7ddd0b1f1d77086c146c6dc9ac5c206ae7ee317d
Kinder-Eggs <g234720@dac.unicamp.br>
2020-04-19 14:06:33 -0300

    Fixed main class, added Table.printTable()

--- src/Table.java
+++ src/Table.java
@@ -1,3 +1,28 @@
 public class Table {
-    int positions[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
+    int positions[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}; // 1 == Black; 2 == White
+
+    public void printTable() {
+        for(int j = 0; j < 8; j++) {
+            System.out.print(8-j);
+            if (j % 2 == 0) {
+                System.out.print(" ");
+            } else {
+                System.out.print("  -");
+            }
+            for (int i = 0; i < 4; i++) {
+                if(positions[i+4*j] == 0) {
+                    System.out.print(" -");
+                } else if(positions[i+4*j] == 1) {
+                    System.out.print(" P");
+                } else {
+                    System.out.print(" B");
+                }
+                if( j % 2 == 0 || i != 3) {
+                    System.out.print(" -");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("   a b c d e f g h");
+    }
 }
---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        CSVReader csv = new CSVReader();
+        //csv.setDataSource("C:/Users/Gasni/IdeaProjects/Damas/src/db/arq001.csv");
+        //String commands[] = csv.requestCommands();
+        Table table = new Table();
+
+        table.printTable();
+    }
+}
